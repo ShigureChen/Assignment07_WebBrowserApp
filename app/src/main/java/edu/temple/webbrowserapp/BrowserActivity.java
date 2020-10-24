2046cd2ec8096c0f702a1e9d92359752a4cc26ab
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-10-25 05:10:20 +0800

    Complete all layout

--- app/src/main/java/edu/temple/webbrowserapp/BrowserActivity.java
+++ app/src/main/java/edu/temple/webbrowserapp/BrowserActivity.java
@@ -3,8 +3,11 @@ package edu.temple.webbrowserapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class BrowserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
